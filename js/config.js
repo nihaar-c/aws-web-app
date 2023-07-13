facb75f50207b8d1d683b059cd432cba913e45a9
nihaar-c <66896038+nihaar-c@users.noreply.github.com>
2023-07-13 16:17:06 -0700

    Add files via upload

---
+++ js/config.js
@@ -0,0 +1,10 @@
+window._config = {
+    cognito: {
+        userPoolId: 'us-west-1_IRADw15tj', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '6eko079eptv99l1961pq9bhihn', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'us-west-1' // e.g. us-east-2
+    },
+    api: {
+        invokeUrl: 'https://9go5c5ymnk.execute-api.us-west-1.amazonaws.com/prod' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
+    }
+};
